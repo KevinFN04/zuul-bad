5b5ee44fb54c3ed0c9a34ad80f3988b21b4feb23
KevinFN04 <kevin.felix.04@gmail.com>
2016-04-14 12:10:53 +0200

    0116 Final:Añadido comando eat, para que el jugador pueda comer

--- Game.java
+++ Game.java
@@ -140,7 +140,9 @@ public class Game
         else if (commandWord.equals("look")) {
             System.out.println(currentRoom.getLongDescription());
         }
-
+        else if (commandWord.equals("eat")) {
+            System.out.println("You have eaten now and you are not hungry any more");
+        }
         return wantToQuit;
     }
 
@@ -210,4 +212,5 @@ public class Game
     private void printLocationInfo(){
         System.out.println(currentRoom.getLongDescription());
     }
+
 }
