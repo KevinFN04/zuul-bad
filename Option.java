dc3eecb576bced5ee03c251656fc43ac9c23a5bd
KevinFN04 <kevin.felix.04@gmail.com>
2016-04-29 13:14:44 +0200

    Ahora los objetos de Option tienen un atributo String

--- Option.java
+++ Option.java
@@ -6,6 +6,16 @@
  */
 public enum Option
 {
-    go, quit, help, look, eat, back, take, drop, items, UNKNOWN    
+    go("ir"), quit("quitar"), help("ayuda"), look("mirar"), eat("comer"), back("retroceder"), take("coger"), drop("tirar"), items("inventario"), UNKNOWN("UNKNOWN");    
+
+    private String command;
+
+    /**
+     * Constructor de la clase Option
+     */
+    Option (String command) 
+    {
+        this.command = command;
+    }
 }
 
