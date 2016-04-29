6f2c58aa717baa05e4b30c30d32672096ff73c3d
KevinFN04 <kevin.felix.04@gmail.com>
2016-04-29 13:04:44 +0200

    Cambiados los comandos al español por comodidad

--- Game.java
+++ Game.java
@@ -109,7 +109,7 @@ public class Game
         System.out.println();
         System.out.println("Welcome to the World of Zuul!");
         System.out.println("World of Zuul is a new, incredibly boring adventure game.");
-        System.out.println("Type 'aiuto' if you need help.");
+        System.out.println("Type 'ayuda' if you need help.");
         System.out.println();
         System.out.println("Abres los ojos y no reconoces la sala que tienes alrededor... Estas en una celda, con barrotes viejos y oxidados ");
         System.out.println("al mirar al techo ves un agujero por el que salir, sin pensarlo saltas fuera de la celda hasta una sala con paredes ");
